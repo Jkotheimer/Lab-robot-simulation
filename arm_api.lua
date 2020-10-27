2018daf7b20737934f06c88619f55a26949c70dc
jkotheimer <jkotheimer@luc.edu>
2020-10-27 01:48:20 -0500

    Cleaned up code for arm api, begun work on hand methods

--- arm_api.lua
+++ arm_api.lua
@@ -1,35 +1,45 @@
 -- This is a threaded script
--- Here are our joint handles
-joints={-1,-1,-1,-1,-1,-1,}
-joints[1]=sim.getObjectHandle('J0')
-joints[2]=sim.getObjectHandle('J1')
-joints[3]=sim.getObjectHandle('J2')
-joints[4]=sim.getObjectHandle('J3')
-joints[5]=sim.getObjectHandle('J4')
-joints[6]=sim.getObjectHandle('J5')
+
+-- ----------------------------------------------------------------------------
+-- GLOBAL ARM-SPECIFIC VARIABLES
+-- ---------------------------------------------------------------------------
+-- Set the RML vectors by converting meters/second to angular equivalent (the lists are to specify values for each joint
+w = math.pi/180
+v = 10 * w
+vel = {v, v, v, v, v, v}
+a = 40 * w
+accel = {a, a, a, a, a, a}
+j = 80 * w
+jerk = {j, j, j, j, j, j}
+
+-- ----------------------------------------------------------------------------
+-- ARM JOINT SETUP
+-- ----------------------------------------------------------------------------
+joints = {-1, -1, -1, -1, -1, -1}
+joints[1] = sim.getObjectHandle('J0')
+joints[2] = sim.getObjectHandle('J1')
+joints[3] = sim.getObjectHandle('J2')
+joints[4] = sim.getObjectHandle('J3')
+joints[5] = sim.getObjectHandle('J4')
+joints[6] = sim.getObjectHandle('J5')
 
 -- stop is used to halt the motion of all motors. The robot starts in stop position.
-stop={0,0,0,0,0,0,0}
-current_vel=stop
-current_accel=stop
--- Set some of the RML vectors by converting to angular equivalent
-vel=10
-vel={vel*math.pi/180,vel*math.pi/180,vel*math.pi/180,vel*math.pi/180,vel*math.pi/180,vel*math.pi/180}
-accel=40
-accel={accel*math.pi/180,accel*math.pi/180,accel*math.pi/180,accel*math.pi/180,accel*math.pi/180,accel*math.pi/180}
-jerk=80
-jerk={jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180}
+stop_arm = {0, 0, 0, 0, 0, 0}
+arm_vel = stop_arm
+arm_accel = stop_arm
 
+-- ----------------------------------------------------------------------------
+-- ARM JOINT API METHODS
+-- ----------------------------------------------------------------------------
 -- Cheap & easy way to call the sim API without copying all this crap every time asdfqwerasdfzxcv
 function move_to(target_vector)
-    sim.rmlMoveToJointPositions(joints,sim.rml_phase_sync_if_possible,current_vel,current_accel,vel,accel,jerk,target_vector,stop)
+    sim.rmlMoveToJointPositions(joints,sim.rml_phase_sync_if_possible,current_vel,current_accel,vel,accel,jerk,target_vector,stop_arm)
 end
 
 -- Return to the origin upright position
 function origin()
     move_to({0,0,0,0,0,0})
 end
-
 -- rot_[a][n]: Rotate the nth motor from the bottom (which rotates about axis 'a') to a target position in degrees
 function rot_z1(target)
     move_to({target*math.pi/180,0,0,0,0,0})
@@ -50,7 +60,23 @@ function rot_y1(target)
     move_to({0,0,0,0,target*math.pi/180,0})
 end
 
+-- HAND API SETUP
+fingers = {-1,-1,-1,-1}
+fingers[1] = sim.getObjectHandle('LEFT_BOTTOM')
+fingers[2] = sim.getObjectHandle('LEFT_TIP')
+fingers[3] = sim.getObjectHandle('RIGHT_BOTTOM')
+fingers[4] = sim.getObjectHandle('RIGHT_TIP')
+
+stop_hand = {0, 0, 0, 0}
+hand_vel = stop_hand
+hand_accel = stop_hand
+
+function close_hand()
+    sim.rmlMoveToJointPositions(fingers,sim.rml_phase_sync_if_possible,current_vel,current_accel,vel,accel,stop_hand,stop_hand)
+end
+
 -- Main thread of execution for this task
 function sysCall_threadmain()
     rot_y1(90)
+	origin()
 end
